6ebd9c23a2b397e079aa8f47140efadd7a45e868
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2022-11-13 15:37:13 +0300

    first 4 questions

---
+++ main.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include "birinci_soru.c"
+#include "ikinci_soru.c"
+#include "ucuncu_soru.c"
+#include "dorduncu_soru.c"
+
+int main() {
+    //printf("hello world");
+//  birinci_soru();
+//  ikinci_soru();
+//  ucuncu_soru();
+    dorduncu_soru();
+
+
+    return 0;
+}
+
+
+
