b66c8662d8cff33b1f341ed0ab0f028421f9de5d
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2023-01-08 16:26:07 +0300

    soru 4 done ✅✅

--- odev_03/soru_04.c
+++ odev_03/soru_04.c
@@ -8,28 +8,75 @@ vb.) Programınız aşağıdaki gibi sorulara cevap vermelidir.
 */
 #include <stdio.h>
 #include <string.h>
-int soru_04() {
+void soru_04() {
   char birim[10];
   float sayi;
+  puts("Desteklenen birimler:");
+  puts("- metre\t->\tince\n- ince\t->\tmetre\n- metre\t->\tsantimetre\n- santimetre->\tmetre\n- litre\t->\tkuart\n- kuart\t->\tlitre\n- gram\t->\tons\n- ons\t->\tgram");
   printf("Sayi giriniz: ");
   scanf("%f", &sayi);
-  printf("Birim giriniz: ");
+  printf("Hangi Birimddan giriniz: ");
   scanf("%s", birim);
-  if (strcmp(birim, "metre") == 0) {
-		printf("%f metre %f ince yapar\n", sayi, sayi*39.37);
-	} else if (strcmp(birim, "ince") == 0) {
-		printf("%f ince %f metre yapar\n", sayi, sayi/39.37);
-	} else if (strcmp(birim, "litre") == 0) {
-		printf("%f litre %f kuart yapar\n", sayi, sayi*1.057);
-	} else if (strcmp(birim, "kuart") == 0) {
-		printf("%f kuart %f litre yapar\n", sayi, sayi/1.057);
-	} else if (strcmp(birim, "gram") == 0) {
-		printf("%f gram %f ons yapar\n", sayi, sayi*0.035);
-	} else if (strcmp(birim, "ons") == 0) {
-		printf("%f ons %f gram yapar\n", sayi, sayi/0.035);
-	} else {
-		printf("Birim bulunamadi");
-	}
-	return 0;
+  printf("Hangi Birime cevirmek istiyorsunuz: ");
+  char birim2[10];
+  scanf("%s", birim2);
+
+  // eger birim2 birimden farkli olmazsa
+  if (strcmp(birim, birim2) == 0 ) {
+	printf("%.2f %s %.2f %s yapar\n", sayi, birim, sayi, birim2);
+	return;
+  } 
+
+  // metre -> inç
+  if (strcmp(birim, "metre") == 0 && strcmp(birim2, "ince") == 0) {
+	printf("%.2f metre %.2f ince yapar\n", sayi, sayi*39.37);
+	return;
+  } 
+  // inç -> metre
+  if (strcmp(birim, "ince") == 0 && strcmp(birim2, "metre") == 0) {
+	printf("%.2f ince %.2f metre yapar\n", sayi, sayi/39.37);
+	return;
+  } 
+
+  // metre -> santimetre
+  if (strcmp(birim, "metre") == 0 && strcmp(birim2, "santimetre") == 0) {
+      printf("%.2f metre %.2f santimetre yapar\n", sayi, sayi*100);
+    return;
+  }
+
+  // santimetre -> metre
+  if (strcmp(birim, "santimetre") == 0 && strcmp(birim2, "metre") == 0) {
+	printf("%.2f santimetre %.2f metre yapar\n", sayi, sayi/100);
+	return;
+  }
+
+  // litre -> kuart
+  if (strcmp(birim, "litre") == 0 && strcmp(birim2, "kuart") == 0) {
+    printf("%.2f litre %.2f kuart yapar\n", sayi, sayi*1.057);
+    return; 
+  } 
+
+  // kuart -> litre
+  if (strcmp(birim, "kuart") == 0 && strcmp(birim2, "litre") == 0) {
+      printf("%.2f kuart %.2f litre yapar\n", sayi, sayi/1.057);
+      return; 
+  } 
+
+  // gram -> ons
+  if (strcmp(birim, "gram") == 0 && strcmp(birim2, "ons") == 0) {
+	printf("%.2f gram %.2f ons yapar\n", sayi, sayi*0.035);
+	return;
+  } 
+
+  // ons -> gram
+  if (strcmp(birim, "ons") == 0 && strcmp(birim2, "gram") == 0) {
+  	printf("%.2f ons %.2f gram yapar\n", sayi, sayi/0.035);
+    return;
+  } 	
+
+
+
+  printf("Birim bulunamadi, sadece (metre, ince, litre, kuart, gram, ons) giriniz.\n");
+  return;
  // fgets(soru, 100, stdin);
 }
